a0c8d2c1b0aa43c0caf8e02661e2ad4732d3619f
Omor Faruk Shadhin <shadhin@Omors-MacBook-Air.local>
2023-11-20 12:30:17 +0600

    3 function replaced in one function in zod-validation middleware

--- back-end/src/services/user.services.ts
+++ back-end/src/services/user.services.ts
@@ -1,90 +1,72 @@
-import { IUserInterface } from "../interfaces/user.interfaces"
+import { IUserInterface } from "../interfaces/user.interfaces";
 
-import { PrismaClient } from '@prisma/client'
+import { PrismaClient } from "@prisma/client";
 import logger from "../utls/logger";
-const prisma = new PrismaClient()
+const prisma = new PrismaClient();
 
-const saveUser=async(data: IUserInterface)=>
-{
-    try
-    {
+const saveUser = async (data: IUserInterface) => {
+  try {
+    // DANGER ZONE
+    // await prisma.user.deleteMany();
 
-        // DANGER ZONE
-        // await prisma.user.deleteMany();
+    const { firstName, lastName, username, email, password, image } = data;
 
+    const user = await prisma.user.create({
+      data: {
+        firstName: firstName,
+        lastName: lastName,
+        username: username,
+        email: email,
+        password: password,
+        image: image,
+      },
+      select: {
+        firstName: true,
+        lastName: true,
+        username: true,
+        image: true,
+        email: true,
+      },
+    });
+    return user;
+  } catch (error: any) {
+    logger.error(error.message);
+    return null;
+  }
+};
 
-        const { firstName, lastName, username, email, password, image }=data;
-        
-        const user= await prisma.user.create(
-        {data: 
-            {
-                firstName: firstName,
-                lastName: lastName,
-                username: username,
-                email: email,
-                password: password,
-                image: image
-            },
-            select:
-            {
-                firstName: true,
-                lastName: true,
-                username: true,
-                image: true,
-                email: true
-            }
-        })
-        return user;
-    }
-    catch(error: any)
-    {
-        logger.error(error.message);
-        return null;
-    }
-}
+const findAllusers = async () => {
+  try {
+    const allUsers = prisma.user.findMany({
+      select: { firstName: true, lastName: true, username: true, email: true },
+    });
+    // console.log(allUsers)
+    return allUsers;
+  } catch (error: any) {
+    logger.error(error.message);
+    return null;
+  }
+};
+const findUserByUsername = async (target: string) => {
+  try {
+    const user = await prisma.user.findUnique({
+      where: {
+        username: target,
+      },
+      select: {
+        firstName: true,
+        lastName: true,
+        username: true,
+        email: true,
+        image: true,
+      },
+    });
+    return user;
+  } catch (error: any) {
+    logger.error(error.message);
+    return null;
+  }
+};
 
-const findAllusers=async()=>
-{
-    try 
-    {
-        const allUsers=prisma.user.findMany();
-        // console.log(allUsers)
-        return allUsers;
-    } 
-    catch(error: any) 
-    {
-        logger.error(error.message);
-        return null
-    }
-}
-const findUserByUsername=async(target: string)=>
-{
-    try 
-    {
-        const user=await prisma.user.findUnique(
-            {
-                where:
-                {
-                    username: target
-                },
-                select:
-                {
-                    firstName: true,
-                    lastName: true,
-                    username: true,
-                    email: true,
-                    image: true
-                }
-            }
-        );
-        return user;
-    } 
-    catch(error: any) 
-    {
-        logger.error(error.message);
-        return null
-    }
-}
-
-const Userservices={ saveUser, findAllusers, findUserByUsername };
+const Userservices = { saveUser, findAllusers, findUserByUsername };
 export default Userservices;
--- back-end/src/zod-interfaces/zod-interfaces.ts
+++ back-end/src/zod-interfaces/zod-interfaces.ts
@@ -3,7 +3,7 @@ import { z } from "zod";
 const UserSchema = z.object({
   firstName: z.string().min(2).max(20),
   lastName: z.string().min(2).max(20),
-  username: z.string().min(5).max(16),
+  username: z.string().min(3).max(16),
   email: z.string().email().min(6).max(30),
   image: z.string(),
   password: z.string().min(6).max(20),
@@ -18,9 +18,9 @@ const RentPostSchema = z.object({
   heading: z.string().min(5).max(20),
   description: z.string().min(10).max(50),
   rent: z.number().min(0),
-  bed: z.number().min(1),
-  bath: z.number().min(1),
-  size: z.number(),
+  bed: z.number().int().min(1),
+  bath: z.number().int().min(1),
+  size: z.number().int().min(1),
   image: z.string(),
 });
 
